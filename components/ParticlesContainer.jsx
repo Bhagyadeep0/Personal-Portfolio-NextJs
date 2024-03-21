795ddbe60b64fe5e87815a7e0f68f44c17084260
Bhagyadeep <b.ghosh0330@gmail.com>
2024-03-21 20:25:06 +0530

    tsparticles

--- components/ParticlesContainer.jsx
+++ components/ParticlesContainer.jsx
@@ -1,3 +1,9 @@
+// ts particles
+import {Particles} from "react-tsparticles";
+import {loadfull} from "tsparticles";
+import React,{useCallback} from "react";
+
+
 const ParticlesContainer = () => {
   return <div>Particles Container</div>;
 };
