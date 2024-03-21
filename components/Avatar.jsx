03b6b8f3181a68d5c3728cf44b96c08c467e2959
Bhagyadeep <b.ghosh0330@gmail.com>
2024-03-21 20:24:25 +0530

    new avatar

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Avatar = () => {
   return (
-    <div className=" absolute bottom-0 right-0 hidden xl:flex xl:max-w-none pointer-events-none">
+    <div className=" hidden xl:flex xl:max-w-none pointer-events-none">
       <Image
         src= {'/avatar.png'}
         width={737}
